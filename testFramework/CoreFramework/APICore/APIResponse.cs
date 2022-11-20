f54a0f8e4420c1a74494d9861247e004c7e388b0
TuNguyen0300 <nguyenhuutu21032000@gmail.com>
2022-11-21 01:01:24 +0700

    TestFramework

---
+++ testFramework/CoreFramework/APICore/APIResponse.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreFramework.APICore
+{
+    public class APIResponse
+    {
+        public HttpWebResponse response;
+        public string responseBody { get; set; }
+        public string responseStatusCode { get; set; }
+        public APIResponse(HttpWebResponse response)
+        {
+            this.response = response;
+            GetResponseBody();
+            GetResponseStatusCode();
+        }
+
+        private string GetResponseStatusCode()
+        {
+            responseBody = "";
+            using (var responseStream = response.GetResponseStream())
+            {
+                if (responseStream != null)
+                    using (var reader = new StreamReader(responseStream))
+                    {
+                        responseBody = reader.ReadToEnd();
+                    }
+            }
+            return responseBody;
+        }
+
+        private string GetResponseBody()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
