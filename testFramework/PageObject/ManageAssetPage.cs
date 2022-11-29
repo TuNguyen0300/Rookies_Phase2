333d61ea8f9c2a7d3089ee7d3090a08bdaf494d3
TuNguyen0300 <nguyenhuutu21032000@gmail.com>
2022-11-29 22:20:04 +0700

    first commit

---
+++ testFramework/PageObject/ManageAssetPage.cs
@@ -0,0 +1,23 @@
+﻿using CoreFramework.DriverCore;
+using OpenQA.Selenium;
+
+namespace testFramework.PageObject
+{
+    public class ManageAssetPage : WebDriverAction
+    {
+        public ManageAssetPage(IWebDriver driver) : base(driver)
+        {
+        }
+
+        private readonly string btnCreateNewAsset = "//span[text()='Create new asset']";
+
+        private readonly string boxAssetName = "//input[@placeholder='Asset Name']";
+        private readonly string boxCategory = "//div[@class='ant-select-selector']";
+        private readonly string boxSpecifi = "//*[@placeholder='Specifications']";
+        private readonly string boxDate = "//*[@placeholder='Select date']";
+        public void ClickCreate()
+        {
+            Click(btnCreateNewAsset);
+        }
+    }
+}
---
+++ testFramework/Testcases/MangageAssetsTest.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using testFramework.Common;
+using testFramework.PageObject;
+using testFramework.TestSetup;
+
+namespace testFramework.Testcases
+{
+    [TestFixture]
+    public class MangageAssetsTest : ProjectNUnitTestSetup
+    {
+        [Test]
+        public void CreateNewAsset()
+        {
+            CommonFlow commonFlow = new CommonFlow();
+            MenuLeft menuLeft = new MenuLeft(_driver);
+            ManageAssetPage manageAssetPage = new ManageAssetPage(_driver);
+
+            commonFlow.CMUserLogin(_driver);
+
+            menuLeft.ClickManageAsset();
+
+            manageAssetPage.ClickCreate();
+        }
+
+    }
+}
